ad27942b04d2cb7de159a3bae6cbdbbb3041a301
dynalz <lkxolp@gmail.com>
2026-01-02 16:21:34 +0000

    build [ci]: version automation

--- cachify/__init__.py
+++ cachify/__init__.py
@@ -1,10 +1,12 @@
+from importlib.metadata import version
+
 from .features.never_die import clear_never_die_registry
 from .memory_cache import cache
 from .redis import DEFAULT_KEY_PREFIX, get_redis_config, reset_redis_config, setup_redis_config
 from .redis_cache import redis_cache
 from .types import CacheKwargs
 
-__version__ = "0.1.0"
+__version__ = version("cachify")
 
 rcache = redis_cache
 
